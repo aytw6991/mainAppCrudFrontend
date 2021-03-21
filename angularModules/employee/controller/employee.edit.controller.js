0f12e8673bcc92585baf37a932a8f0f3976bf85c
ayush-tiwari7 <iush.twry@gmail.com>
2021-03-21 07:22:22 +0545

    added codes

--- angularModules/employee/controller/employee.edit.controller.js
+++ angularModules/employee/controller/employee.edit.controller.js
@@ -4,29 +4,43 @@
     angular.module('mainApp.employee')
             .controller('EmployeeEditController', EmployeeEditController);
 
-    EmployeeEditController.$inject = ['employeeService', '$stateParams', '$state'];
+    EmployeeEditController.$inject = ['employeeService', '$stateParams', '$state', 'unitService'];
 
-    function EmployeeEditController(employeeService, $stateParams, $state){
+    function EmployeeEditController(employeeService, $stateParams, $state, unitService){
         var vm = this;
 
         vm.childBack = childBack;
         vm.editUser = editUser;
         vm.getById = getById;
+        vm.getUnits = getUnits;
         vm.id = $stateParams.id
+        vm.units = [];
         vm.user = {};
 
         activate();
 
         function activate(){
             getById();
+            getUnits();
         };
 
         function childBack(){
-            $state.go('^.view');
+            return $state.go('^.view');
         };
 
         function editUser(){
-            return employeeService.editUser(vm.id, vm.user);
+            return employeeService.editUser(vm.id, vm.user)
+                .then(function(){
+                    return $state.go('^.view');
+                });
+        };
+
+        function getUnits(){
+            return unitService.getUnits()
+                .then(function(data){
+                    vm.units = data;
+                    return vm.units;
+                });
         };
 
         function getById(){
@@ -34,7 +48,7 @@
                 .then(function(data){
                     vm.user = data;
                 });
-        };
+        };  
     };
 
 })();
