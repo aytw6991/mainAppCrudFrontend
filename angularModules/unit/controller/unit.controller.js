6e072313c45a171433c8a98674bc29b3fb890048
ayush-tiwari7 <iush.twry@gmail.com>
2021-03-21 07:23:40 +0545

    removed redundant lines

--- angularModules/unit/controller/unit.controller.js
+++ angularModules/unit/controller/unit.controller.js
@@ -12,18 +12,18 @@
         vm.childBack = childBack;
         vm.createUnit = createUnit;
         vm.deleteUnit = deleteUnit;
-        vm.editUnit = editUnit;
-        vm.getById = getById;
+        // vm.editUnit = editUnit;
+        // vm.getById = getById;
         vm.getUnits = getUnits;
         vm.id = $stateParams.id;
         vm.parentBack = parentBack;
-        vm.unit = {};
+        // vm.unit = {};
         vm.units = [];
 
         activate();
 
         function activate(){
-            getById();
+            // getById();
             getUnits();
         };
 
@@ -45,19 +45,19 @@
                 });
         };
 
-        function editUnit(){
-            return unitService.editUnit(vm.id, vm.unit)
-                .then(function(){
-                    $state.go('^.view');
-                });
-        };
+        // function editUnit(){
+        //     return unitService.editUnit(vm.id, vm.unit)
+        //         .then(function(){
+        //             $state.go('^.view');
+        //         });
+        // };
 
-        function getById(){
-            return unitService.getById(vm.id)
-                .then(function(data){
-                    vm.unit = data;
-                });
-        };
+        // function getById(){
+        //     return unitService.getById(vm.id)
+        //         .then(function(data){
+        //             vm.unit = data;
+        //         });
+        // };
 
         function getUnits(){
             return unitService.getUnits()
