c12f891081a1b5abbfe26f3bfbb24e00dfb5d74f
ayush-tiwari7 <iush.twry@gmail.com>
2021-03-21 14:09:35 +0545

    Added user drop down in attedance logic

--- angularModules/attendance/controller/attendance.controller.js
+++ angularModules/attendance/controller/attendance.controller.js
@@ -4,21 +4,25 @@
     angular.module('mainApp.attendance')
             .controller('AttendanceController', AttendanceController);
 
-    AttendanceController.$inject = ['attendanceService', '$state'];
+    AttendanceController.$inject = ['attendanceService', 'employeeService', '$state'];
 
-    function AttendanceController(attendanceService, $state){
+    function AttendanceController(attendanceService, employeeService, $state){
         var vm = this;
 
         vm.childBack = childBack;
         vm.checkIn = checkIn;
         vm.getAll = getAll;
+        vm.getUsers = getUsers;
+        vm.loggedData = [];
         vm.parentBack = parentBack;
-        vm.times = [];
+        vm.user = {};
+        vm.users = [];
 
         activate();
 
         function activate(){
             getAll();
+            getUsers();
         };
 
         function childBack(){
@@ -26,19 +30,29 @@
         };
         
         function checkIn(){
-            return attendanceService.checkIn();
+            return attendanceService.checkIn(vm.user);
         };
 
+        //get all attendance data
         function getAll(){
             return attendanceService.getAll()
                 .then(function(data){
-                    vm.times = data;
-                    return vm.times;
+                    vm.loggedData = data;
+                    return vm.loggedData;
+                });
+        };
+
+        //get users for dropdown
+        function getUsers(){
+            return employeeService.getUsers()
+                .then(function(data){
+                    vm.users = data;
+                    return vm.getUsers;
                 });
         };
 
         function parentBack(){
             $state.go('^');
         };
-    }
+    };
 })();
